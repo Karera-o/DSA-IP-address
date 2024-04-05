eb8f6fc990556cd8b4d0ca877182c737acc1a87c
Karera-o <olivierkarera2020@gmail.com>
2024-04-05 19:17:02 +0200

    wip

--- code/src/IPAddressAnalyze.cpp
+++ code/src/IPAddressAnalyze.cpp
@@ -4,113 +4,124 @@
  */
 #include "IPAddressAnalyze.h"
 
-int IPAddressAnalyzer::readNextItemFromFile(FILE* inputFileStream){
-	if (! inputFileStream){
-		std::string message("Cannot open input file for reading");
-		throw std::invalid_argument(message.c_str());
-	}
+#define MAX_IPS 10000
 
+#define MAX_IP_LENGTH 16
+
+int IPAddressAnalyzer::readNextItemFromFile(FILE* inputFileStream){
+    if (!inputFileStream){
+        std::string message("Cannot open input file for reading");
+        throw std::invalid_argument(message.c_str());
+    }
+
+    return -1;
+}
+
+struct IPData {
+    char ip[MAX_IP_LENGTH];
+    unsigned int count;
+} ipData[MAX_IPS];
+
+unsigned int totalIPs = 0;
+
+
+void manualStrcpy(char* dest, const char* src) {
+    while ((*dest++ = *src++));
 }
 
 
-// void IPAddressAnalyzer::getMostFrequentIPAddress(char* inputFilePath, char* outputFilePath, int n){
-// 	FILE* inFileStream = fopen(inputFilePath, "r");
-// 	if (! inFileStream){
-// 		char message[1024];
-// 		sprintf(message, "Cannot open input file for reading: %s", inputFilePath);
-// 		throw std::ios_base::failure(message);
-// 	}
-
-// 	FILE* outFileStream = fopen(outputFilePath, "w");
-// 	if (! outFileStream){
-// 		char message[1024];
-// 		sprintf(message, "Cannot open output file for writing: %s", outputFilePath);
-// 		throw std::ios_base::failure(message);
-// 	}
-
-// 	LogManager::writePrintfToLog(LogManager::Level::Status,
-// 			"IPAddressAnalyzer::getMostFrequentIPAddress",
-// 			"Starting to process file %s", inputFilePath);
-
-// 	fflush(outFileStream);
-// 	fclose(outFileStream);
-
-// 	fclose(inFileStream);
-// }
-
-struct IPAddressCount {
-    std::string ip;
-    int count;
-};
-
-// Function prototypes
-int indexOfIP(IPAddressCount* ipCounts, int size, std::string ip);
-void insertSorted(IPAddressCount* ipCounts, IPAddressCount newIPCount, int& size, int maxSize);
-
-void IPAddressAnalyzer::getMostFrequentIPAddress(char* inputFilePath, char* outputFilePath, int n) {
-    std::ifstream inputFile(inputFilePath);
-    if (!inputFile.is_open()) {
-        std::cerr << "Cannot open input file for reading\n";
-        return;
+int manualStrcmp(const char* str1, const char* str2) {
+    while (*str1 && (*str1 == *str2)) {
+        str1++;
+        str2++;
     }
+    return (unsigned char)(*str1) - (unsigned char)(*str2);
+}
 
-    std::ofstream outputFile(outputFilePath);
-    if (!outputFile.is_open()) {
-        std::cerr << "Cannot open output file for writing\n";
-        return;
-    }
-
-    IPAddressCount* ipCounts = new IPAddressCount[10000]; // Arbitrary size for demonstration
-    int totalIPs = 0; // Tracks the total number of unique IPs encountered
-
-    std::string line;
-    while (getline(inputFile, line)) {
-        std::string ip = line.substr(0, line.find(","));
-        int count = std::stoi(line.substr(line.find(",") + 1));
-
-        int index = indexOfIP(ipCounts, totalIPs, ip);
-        if (index != -1) { // IP found, update count
-            ipCounts[index].count += count;
-            // Ensure the list remains sorted after update
-            insertSorted(ipCounts, ipCounts[index], totalIPs, 10000);
-        } else { // New IP, add to list
-            if (totalIPs < 10000) { // Prevent overflow
-                IPAddressCount newIPCount = {ip, count};
-                insertSorted(ipCounts, newIPCount, totalIPs, 10000);
+void addOrUpdateIP(const char* ip, unsigned int count) {
+    for (unsigned int i = 0; i < totalIPs; ++i) {
+        
+        bool isSameIP = true;
+        for (int j = 0; ip[j] != '\0' || ipData[i].ip[j] != '\0'; ++j) {
+            if (ip[j] != ipData[i].ip[j]) {
+                isSameIP = false;
+                break;
             }
         }
-    }
-
-    // Output the top n IPs
-    for (int i = 0; i < n && i < totalIPs; ++i) {
-        outputFile << i+1 << ", " << ipCounts[i].ip << ", " << ipCounts[i].count << "\n";
-    }
-
-    delete[] ipCounts;
-    inputFile.close();
-    outputFile.close();
-}
-
-int indexOfIP(IPAddressCount* ipCounts, int size, std::string ip) {
-    for (int i = 0; i < size; ++i) {
-        if (ipCounts[i].ip == ip) {
-            return i;
+        if (isSameIP) {
+            ipData[i].count += count;
+            return;
         }
     }
-    return -1; // Not found
+    if (totalIPs < MAX_IPS) {
+        manualStrcpy(ipData[totalIPs].ip, ip);
+        ipData[totalIPs].count = count;
+        totalIPs++;
+    }
 }
 
-void insertSorted(IPAddressCount* ipCounts, IPAddressCount newIPCount, int& size, int maxSize) {
-    int i;
-    for (i = size - 1; (i >= 0 && ipCounts[i].count < newIPCount.count); i--) {
-        if (i < maxSize - 1) {
-            ipCounts[i + 1] = ipCounts[i];
+void sortIPData() {
+
+    for (unsigned int i = 1; i < totalIPs; i++) {
+        IPData key = ipData[i];
+        int j = i - 1;
+
+
+        while (j >= 0 && (ipData[j].count < key.count || 
+                         (ipData[j].count == key.count && manualStrcmp(ipData[j].ip, key.ip) < 0))) {
+            ipData[j + 1] = ipData[j];
+            j--;
+        }
+        ipData[j + 1] = key;
+    }
+}
+
+
+void IPAddressAnalyzer::getMostFrequentIPAddress(char* inputFilePath, char* outputFilePath, int n) {
+    FILE* inFileStream = fopen(inputFilePath, "r");
+    if (!inFileStream) {
+        fprintf(stderr, "Cannot open input file for reading: %s\n", inputFilePath);
+        return;
+    }
+
+    FILE* outFileStream = fopen(outputFilePath, "w");
+    if (!outFileStream) {
+        fprintf(stderr, "Cannot open output file for writing: %s\n", outputFilePath);
+        fclose(inFileStream);
+        return;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), inFileStream)) {
+        char ip[MAX_IP_LENGTH] = {0};
+        unsigned int count;
+        sscanf(line, "%[^,], %u", ip, &count);
+        addOrUpdateIP(ip, count);
+    }
+
+    fclose(inFileStream); 
+
+    sortIPData();
+
+    int outputCount = 0;
+    int currentRank = 1;
+    unsigned int lastCount = UINT_MAX;
+    int rankForNextValidIP = 1;
+
+    for (unsigned int i = 0; i < totalIPs && outputCount < n; ++i) {
+        if (lastCount != ipData[i].count) {
+            currentRank = rankForNextValidIP;
+            lastCount = ipData[i].count;
+        }
+        
+        fprintf(outFileStream, "%d, %s, %u\n", currentRank, ipData[i].ip, ipData[i].count);
+        outputCount++;
+        rankForNextValidIP++;
+
+        if (i + 1 < totalIPs && ipData[i + 1].count == lastCount && outputCount == n) {
+            n++; 
         }
     }
-    if (i < maxSize - 1) {
-        ipCounts[i + 1] = newIPCount;
-    }
-    if (size < maxSize) {
-        size++;
-    }
+
+    fclose(outFileStream); 
 }
